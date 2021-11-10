95cd538e89eee2715b19322abc433678d121d4c3
OjusWiZard <ojuswimail@gmail.com>
2021-11-11 01:21:53 +0530

    added the initial map
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

---
+++ assets/js/map.js
@@ -0,0 +1,19 @@
+---
+---
+
+var ourMap = L.map('map').setView([0, 0], 1);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoib2p1c3dpIiwiYSI6ImNrdnR3djhiaTMxYXMycm91ZTl0cTQ3NjEifQ.K6iJsG5RDE7rZFF8tzel9w'
+}).addTo(ourMap);
+
+var fellows = {{ site.data.fellows | jsonify}};
+fellows.forEach(fellow => {
+    var marker = L.marker([fellow.location.latitude, fellow.location.longitude]).addTo(ourMap);
+    marker.bindPopup(fellow.name);
+});
