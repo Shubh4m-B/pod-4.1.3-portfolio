058a6cc3821f35e07526153acd29edb06ffcd61e
OjusWiZard <ojuswimail@gmail.com>
2021-11-12 01:30:41 +0530

    added profile icon markers
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- assets/js/map.js
+++ assets/js/map.js
@@ -14,8 +14,20 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 var fellows = {{ site.data.fellows | jsonify}};
 
-fellows.forEach((fellow)=>{
-    var marker = L.marker([fellow.location.latitude, fellow.location.longitude]).addTo(ourMap);
+fellows.forEach((fellow) => {
+    var markerIcon = L.icon({
+        iconUrl: './assets/img/'+fellow.img,
+        shadowUrl: './assets/img/shadow.png',
+    
+        iconSize:     [40, 40], // size of the icon
+        shadowSize:   [25, 10], // size of the shadow
+        iconAnchor:   [0, 40], // point of the icon which will correspond to marker's location
+        shadowAnchor: [-15, 0],  // the same for the shadow
+        popupAnchor: [20, -45], // point from which the popup should open relative to the iconAnchor
+        className: 'marker'
+    });
+
+    var marker = L.marker([fellow.location.latitude, fellow.location.longitude], {icon: markerIcon}).addTo(ourMap);
 
     const card = `
       <div class="container">
